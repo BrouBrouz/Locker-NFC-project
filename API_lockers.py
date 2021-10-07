22fd01499c81cf22dc5d7afc15369fc4f09f2f9f
BrouBrouz <72034687+BrouBrouz@users.noreply.github.com>
2021-10-07 20:52:13 +0200

    API return code update

--- API_lockers.py
+++ API_lockers.py
@@ -5,22 +5,18 @@ app = Flask(__name__)
 data = {111111 : {"lockers": [1,2]},
         222222 : {"lockers": [2,3]}}
 
-@app.route("/")
-def home():
-    return "Smart Lock Project", 200
-
 @app.route("/<int:id>/<int:locker>", methods=['GET'])
 def authorise_check(id, locker):
     if (id not in data):
-        return "<h1>404</h1><p>Student not found.</p>", 404
+        return "",404
     elif(locker not in data[id]["lockers"]):
-        return "<h1>401</h1><p>Student not authorized.</p>", 401
+        return "", 401
     else:
-        return "<h1>200</h1><p>Student authorized</p>", 200
+        return "", 200
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+    return "", 404
 
 if __name__ == "__main__":
     app.run(debug=True)
